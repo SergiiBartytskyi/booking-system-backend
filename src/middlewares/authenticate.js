90381ef657e53034527f0ae5211b6987c105c173
Sergii Bartytskyi <s.bartycjkyj@gmail.com>
2025-05-22 23:11:19 +0200

    fix authentiate

--- src/middlewares/authenticate.js
+++ src/middlewares/authenticate.js
@@ -3,26 +3,31 @@ import createHttpError from 'http-errors';
 import { findSession, findUser } from '../services/auth.js';
 
 export const authenticate = async (req, res, next) => {
-  const authHeader = req.get('Authorization');
+  // const authHeader = req.get('Authorization');
 
-  if (!authHeader) {
-    return next(createHttpError(401, 'Please provide Authorization header!'));
-  }
+  // if (!authHeader) {
+  //   return next(createHttpError(401, 'Please provide Authorization header!'));
+  // }
 
-  const [bearer, token] = authHeader.split(' ');
+  // const [bearer, token] = authHeader.split(' ');
 
-  if (bearer !== 'Bearer' || !token) {
-    return next(createHttpError(401, 'Auth header should be of type Bearer!'));
-  }
+  // if (bearer !== 'Bearer' || !token) {
+  //   return next(createHttpError(401, 'Auth header should be of type Bearer!'));
+  // }
+  // const session = await findSession({ accessToken: token });
 
-  const session = await findSession({ accessToken: token });
+  // if (Date.now() > session.accessTokenValidUntil) {
+  //   return next(createHttpError(401, 'Access token expired!'));
+  // }
 
+  const { sessionId } = req.cookies;
+  if (!sessionId) return next(createHttpError(401, 'Unauthorized'));
+  const session = await findSession({ _id: sessionId });
   if (!session) {
     return next(createHttpError(401, 'Session not found!'));
   }
-
-  if (Date.now() > session.accessTokenValidUntil) {
-    return next(createHttpError(401, 'Access token expired!'));
+  if (!session || session.refreshTokenValidUntil < new Date()) {
+    return next(createHttpError(401, 'Session expired'));
   }
 
   const user = await findUser({ _id: session.userId });
